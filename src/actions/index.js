4e01ae0ab6c2302e5ddf3f5b5fb1448d8295efaa
unknown <goodyer@gmail.com>
2018-08-01 13:53:17 -0400

    added first API functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,19 @@
+import axios from 'axios';
+
 const API_KEY = '95798dbe11aaa19a5400a7fa019e8ad9';
-const URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
-//const URL = 'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=95798dbe11aaa19a5400a7fa019e8ad9';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+//http://api.openweathermap.org/data/2.5/forecast?appid=95798dbe11aaa19a5400a7fa019e8ad9&q=London,uk
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 
-export default fetchWeather() {
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
 	return {
-		type: FETCH_WEATHER
+		type: FETCH_WEATHER,
+		payload: request
 	};
 }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-
-export default class SearchBar extends Component {
+//export default 
+class SearchBar extends Component {
 	constructor(props) {
 		super(props);
-
-		
-		
 		this.state = { term: '' }
-
-		this.onInputChange= this.onInputChange.bind(this);
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
-
-	
 
 	onInputChange(event) {
 		//console.log(event.target.value);
 		this.setState({ term: event.target.value });
+		
 	}
 
 	onFormSubmit(event) {
 		event.preventDefault();
+		this.props.fetchWeather(this.state.term);
+		this.setState({ term: '' });
 	}
 
 	render() {
 		return (
 			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input 
-				placeholder="Get a five-day forecase in your favourite cities"
-				className="form-control"
-				value={this.state.term}
-				onChange={this.onInputChange}
+					placeholder="Get a five-day forecase in your favourite cities"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
 				/>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">Submit</button>
@@ -41,3 +42,10 @@ export default class SearchBar extends Component {
 	}
 
 }
+
+
+function mapDispatchToProps(dispatch) {
+ 	return bindActionCreators( { fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
